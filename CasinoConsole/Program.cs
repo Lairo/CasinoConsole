5d28bd2747dac60c82b7f4eeba895066fc13b581
chajus1 <chajus1@googlemail.com>
2025-04-29 11:45:27 +0100

    Added a second player

--- CasinoConsole/Program.cs
+++ CasinoConsole/Program.cs
@@ -4,13 +4,14 @@
     {
         static void Main(string[] args)
         {
-            //Player joe = new Player() { Cash = 50, Name = "Joe" };
-
             Random random = new Random();
-            Player player = new Player() { Cash = 100, Name = "The Player" };
+            Player[] players = new Player[2];
+            players[0] = new Player() { Cash = 100, Name = "Bob" };
+            players[1] = new Player() { Cash = 50, Name = "Joe" };
 
+            Player currentPlayer = new();
             double odds = .75;
-            //int pot;
+
             Console.WriteLine($"Welcome to the casino. The odds are {odds*100}%");
             while (true)
             {
@@ -19,40 +20,65 @@
                 if (input == "") return;
                 if (double.TryParse(input, out double newOdds))
                 {
-                    odds = newOdds/100;
-                    Console.WriteLine($"The new odds are {odds*100}%");
-                    while (player.Cash > 0)
+                    odds = newOdds / 100;
+                    Console.WriteLine($"The new odds are {odds * 100}% \n");
+                    
+                    while (true)
                     {
-
-                        player.WriteMyInfo();
-                        double chance = random.NextDouble();
-
-                        Console.Write("How much do you want to bet: ");
-                        string? howMuch = Console.ReadLine();
-                        if (howMuch == "") return;
-                        if (int.TryParse(howMuch, out int amount))
+                        foreach (Player player in players)
                         {
-                            Console.WriteLine($"You rolled {chance:0.00} vs {odds} chance \n");
-                            if (odds < chance)
+                            
+                            Console.WriteLine($"{player.Name} has {player.Cash} cash.");                           
+                            
+                        }
+                        Console.WriteLine();
+                        Console.Write("Select a player) (1 (Bob), 2 (Joe):  ");
+                        string? playerSelection = Console.ReadLine();
+                        if (playerSelection == "") return;
+                        if (int.TryParse(playerSelection, out int selection))
+                        {
+                            currentPlayer = players[selection - 1];
+                            Console.WriteLine($"You are playing as {currentPlayer.Name}");
+                            Console.WriteLine();
+                            while (currentPlayer.Cash > 0)
                             {
-                                int pot = player.GiveCash(amount) * 2;
-                                player.ReceiveCash(pot);
-                                Console.WriteLine($"You win {pot}");
-                            }
-                            else
-                            {
-                                //int pot = player.GiveCash(amount);
-                                player.GiveCash(amount);
-                                Console.WriteLine("Bad luck, you lose");
+
+                                currentPlayer.WriteMyInfo();
+                                double chance = random.NextDouble();
+
+                                Console.Write("How much do you want to bet: ");
+                                string? howMuch = Console.ReadLine();
+                                if (howMuch == "") return;
+                                if (int.TryParse(howMuch, out int amount))
+                                {
+                                    Console.WriteLine($"You rolled {chance:0.00} vs {odds} chance \n");
+                                    if (odds < chance)
+                                    {
+                                        int pot = currentPlayer.GiveCash(amount) * 2;
+                                        currentPlayer.ReceiveCash(pot);
+                                        Console.WriteLine($"You win {pot}");
+                                    }
+                                    else
+                                    {
+                                        //int pot = player.GiveCash(amount);
+                                        currentPlayer.GiveCash(amount);
+                                        Console.WriteLine("Bad luck, you lose");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Please enter a valid number.");
+                                }
+
                             }
                         }
                         else
                         {
                             Console.WriteLine("Please enter a valid number.");
                         }
-
                     }
                 }
+
                 else
                 {
                     Console.WriteLine("Please enter a valid number.");
