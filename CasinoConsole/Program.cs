682939782f8c36e95842a3f2769054b5929c75f2
chajus1 <chajus1@googlemail.com>
2025-04-29 07:54:13 +0100

    Add project files.

---
+++ CasinoConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace CasinoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player joe = new Player() { Cash = 50, Name = "Joe" };
+            Player bob = new Player() { Cash = 100, Name = "Bob" };
+
+            while (true)
+            {
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
+
+                Console.Write("Enter an amount: ");
+                string? howMuch = Console.ReadLine();
+                if (howMuch == "") return;
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    Console.Write("Who should give the cash: ");
+                    string? whichGuy = Console.ReadLine();
+                    if (whichGuy == "Joe")
+                    {
+                        int result = joe.GiveCash(amount);
+                        bob.ReceiveCash(result);
+
+                    }
+                    else if (whichGuy == "Bob")
+                    {
+                        int result = bob.GiveCash(amount);
+                        bob.ReceiveCash(result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter 'Joe' or 'Bob");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount (or a blank line to exit).");
+                }
+            }
+        }
+    }
+}
